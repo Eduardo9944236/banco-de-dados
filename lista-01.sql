d22ac4553f56c24699d91d61f443832d52adc7de
EduardoF <134980731+Eduardo9944236@users.noreply.github.com>
2024-05-21 16:56:14 -0300

    Add files via upload

---
+++ lista-01.sql
@@ -0,0 +1,5 @@
+use avaliacao_22a;
+
+
+insert into Livros (titulo, autor, ano_publicacao, disponivel, categoria,isbn, editora, quantidade_paginas, idioma) VALUES
+ ("As cronicas de Narnia","C.S Lewis", "1950", TRUE, "Fantasia", "978-0064471190", "HarperCollins", "762", "Ingles") 
